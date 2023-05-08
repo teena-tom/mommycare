2a59b7a4ab91411dde108e83ab77e891f6ef8768
Teena Tom <tomteena7@gmail.com>
2023-05-08 01:58:06 -0700

    successfully added the footer with responsive design and a fixed position

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import "./App.scss";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Header />
-        Hello
+       <div className="App__body">
+       Hello
+       </div>
+        
+        <Footer />
       </BrowserRouter>
     </div>
   );
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import "./Footer.scss";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <p className="footer__text">&copy; MommyCare Inc. All Rights Reserved </p>
+    </div>
+  );
+}
+export default Footer;
