671b8470f029f97dc647d61f145c856003bb1f0f
Teena Tom <tomteena7@gmail.com>
2023-05-15 21:50:23 -0700

    fixed small bugs like warning of unused imports

--- src/pages/DaycarePage/DaycarePage.js
+++ src/pages/DaycarePage/DaycarePage.js
@@ -1,30 +1,29 @@
 import "./DaycarePage.scss";
 import DaycareList from "../../components/DaycareList/DaycareList";
-// import NewWarehouse from "../../components/NewWarehouse/NewWarehouse";
+
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import backArrow from '../../assets/icons/arrow_back-24px.svg'
+import backArrow from "../../assets/icons/arrow_back-24px.svg";
 import DeleteModel from "../../components/DeleteModel/DeleteModel";
-import CtaButton from "../../components/CtaButton/CtaButton";
-import axios from 'axios';
+import axios from "axios";
 
 const baseUrl = process.env.REACT_APP_BASE_URL ?? "http://localhost:5050/api";
 
-
-function DaycarePage({mode = "view"}) {
+function DaycarePage({ mode = "view" }) {
   const [daycares, setDaycares] = useState(null);
   const [deletingId, setDeletingId] = useState(null);
-  const deletingDaycare = daycares?.find(daycare => daycare.id === deletingId);
-
+  const deletingDaycare = daycares?.find(
+    (daycare) => daycare.id === deletingId
+  );
 
   useEffect(() => {
     if (daycares === null) {
       axios
         .get(`${baseUrl}/Daycares`)
-        .then(response => {
+        .then((response) => {
           setDaycares(response.data);
         })
-        .catch(error => {
+        .catch((error) => {
           alert(error);
           console.error(error);
         });
@@ -38,11 +37,11 @@ function DaycarePage({mode = "view"}) {
   const handleDeleteConfirmed = (id) => {
     axios
       .delete(`${baseUrl}/daycares/${deletingId}`)
-      .then(response => {
+      .then((response) => {
         setDeletingId(null);
-        setDaycares(null); //Triggers a warehouse list to reload
+        setDaycares(null); //Triggers a daycare  to reload
       })
-      .catch(error => {
+      .catch((error) => {
         alert(error);
         console.error(error);
       });
@@ -52,44 +51,52 @@ function DaycarePage({mode = "view"}) {
   };
 
   return (
-      
     <main className="Daycares-Page">
       <div className="Daycares-Page__header">
-        {mode === "add" && 
-        <> 
-        <Link to="/">
-          <button className="Daycares-Page__back-button">
-            <img className="Daycares-Page__arrow" src={backArrow} alt="back" />
-          </button>
-        </Link>
-        {/* <h1 className="Daycares-Page__title">Add New Warehouse</h1> */}
-        </>
-        }
-        
-        {mode === "view" &&
-        <>
-         <h1 className="Daycares-Page__title">Daycares</h1>
-        <input type="search" name="search" className="Daycares-Page__search-field" placeholder="Search..." />
-        {/* <Link to="/add"><CtaButton>+ Add New Daycare</CtaButton></Link> */}
-        </>
-        }
+        {mode === "add" && (
+          <>
+            <Link to="/">
+              <button className="Daycares-Page__back-button">
+                <img
+                  className="Daycares-Page__arrow"
+                  src={backArrow}
+                  alt="back"
+                />
+              </button>
+            </Link>
+          </>
+        )}
+
+        {mode === "view" && (
+          <>
+            <h1 className="Daycares-Page__title">Daycares</h1>
+            <input
+              type="search"
+              name="search"
+              className="Daycares-Page__search-field"
+              placeholder="Search..."
+            />
+          </>
+        )}
       </div>
 
-      {mode === "view" && (daycares !== null
-        
-          ? <DaycareList daycares={daycares} onDelete={handleDelete} />
-          : <h2>Loading...</h2>)
-        }
+      {mode === "view" &&
+        (daycares !== null ? (
+          <DaycareList daycares={daycares} onDelete={handleDelete} />
+        ) : (
+          <h2>Loading...</h2>
+        ))}
 
-        {deletingDaycare && <DeleteModel
+      {deletingDaycare && (
+        <DeleteModel
           title={`Delete ${deletingDaycare.daycare_name} daycare?`}
           message={`Please confirm that you'd like to delete ${deletingDaycare.daycare_name} from the list of daycares. You won't be able to undo this action.`}
           onConfirm={handleDeleteConfirmed}
           onCancel={handleDeleteCancelled}
         />
-        } 
+      )}
       {/* {mode === "add" && <NewDaycare />}       */}
-    </main>        
+    </main>
   );
 }
 export default DaycarePage;
